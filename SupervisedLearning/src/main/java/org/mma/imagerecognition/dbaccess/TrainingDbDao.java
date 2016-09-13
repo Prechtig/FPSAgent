1ef94bdf23d5fb1bc0c2b21ac57abe34e2d527f7
Mark <math@schantz.com>
2016-09-13 09:27:51 +0200

    Adds additional functionality to DAO

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/dbaccess/TrainingDbDao.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/dbaccess/TrainingDbDao.java
@@ -12,13 +12,16 @@ import org.mma.imagerecognition.dataobjects.TrainingData;
 import org.mma.imagerecognition.tools.PropertiesReader;
 
 public class TrainingDbDao {
-	private static String databaseURL = "jdbc:mysql://mydb.itu.dk/TrainingDB";
+	private static final String DbName = "TrainingDB";
+	private static final String tableName = "trainingData";
+	private static final String databaseURL = "jdbc:mysql://mydb.itu.dk/" + DbName;
 	private static Connection conn = null;
 	
 	private static PreparedStatement getWidthHeightPS;
 	private static PreparedStatement getRandomTrainingDataPS;
-	private static String GET_WIDTH_HEIGHT = "SELECT width, height FROM trainingData LIMIT 1";
+	private static PreparedStatement getColumnCountPS;
 	
+	private static String ERROR_MESSAGE = "Cannot execute queries before connection has been initialized.";
 	
 	public static void initializeConnection() {
 		try {
@@ -30,30 +33,32 @@ public class TrainingDbDao {
 	
 	public static List<TrainingData> getImages(int rows) {
 		if(conn==null) {
-			throw new IllegalStateException("Cannot execute queries before  onnection has been initialized.");
+			throw new IllegalStateException(ERROR_MESSAGE);
+		}
+		
+		final String GET_RANDOM_IMAGES = "SELECT * FROM trainingData ORDER BY RAND() LIMIT ?";
+		
+		if(getRandomTrainingDataPS == null) {
+			try {
+				getRandomTrainingDataPS = conn.prepareStatement(GET_RANDOM_IMAGES);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		List<TrainingData> trainingSet = new ArrayList<>();
-		String GET_RANDOM_IMAGES = "SELECT * FROM trainingData ORDER BY RAND() LIMIT " + rows; //TODO: Optimize this to use prepared statements correctly
-		PreparedStatement ps = null;
-		
 		try {
-			ps = conn.prepareStatement(GET_RANDOM_IMAGES);
-			ps.execute();
-			ResultSet resultSet = ps.getResultSet();
+			getRandomTrainingDataPS.clearParameters();
+			getRandomTrainingDataPS.setInt(1, rows);
+			
+			getRandomTrainingDataPS.execute();
+			ResultSet resultSet = getRandomTrainingDataPS.getResultSet();
 			
 			while(resultSet.next()) {
 				trainingSet.add(new TrainingData(resultSet));
 			}
-			
-		} catch(Exception e){
-			e.printStackTrace();;
-		}finally {
-			try {
-				ps.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
 		return trainingSet;
@@ -61,8 +66,10 @@ public class TrainingDbDao {
 	
 	public static int getWidthHeightProduct() {
 		if(conn==null) {
-			throw new IllegalStateException("Cannot execute queries before connection has been initialized.");
+			throw new IllegalStateException(ERROR_MESSAGE);
 		}
+		
+		final String GET_WIDTH_HEIGHT = "SELECT width, height FROM trainingData LIMIT 1";
 		
 		if(getWidthHeightPS == null) {
 			try {
@@ -88,5 +95,34 @@ public class TrainingDbDao {
 		}
 		
 		return result;
+	}
+	
+	public static int getNumberOfGroundTruths() {
+		if(conn==null) {
+			throw new IllegalStateException(ERROR_MESSAGE);
+		}
+		
+		final String GET_COLUMN_COUNT = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '" + DbName + "' AND table_name = '" + tableName + "'";
+		
+		if(getColumnCountPS == null) {
+			try {
+				getColumnCountPS = conn.prepareStatement(GET_COLUMN_COUNT);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		int columnCount = 0;
+		
+		try {
+			ResultSet rs = getColumnCountPS.executeQuery();
+			rs.next();
+			columnCount = rs.getInt(1) - 4;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return columnCount;
 	}
 }
