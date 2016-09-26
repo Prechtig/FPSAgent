f3309cf8f7d41b238d906d8f667f2b9c4c8392f1
Mark <markthorh@gmail.com>
2016-09-26 21:59:21 +0200

    Adds simple convnet architecture

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/BuilderFactory.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/BuilderFactory.java
@@ -75,4 +75,55 @@ public class BuilderFactory {
 		new ConvolutionLayerSetup(builder, height, width, 3);
 		return builder;
 	}
+	
+	public static Builder getConvNet(int height, int width, int featureCount) {
+		Builder builder = new NeuralNetConfiguration.Builder()
+		.seed(98)
+		.iterations(1)
+		.regularization(true)
+		.l2(0.0005)
+		.learningRate(0.008)
+		.weightInit(WeightInit.XAVIER)
+		.activation("relu")
+		.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+		.updater(Updater.NESTEROVS).momentum(0.9)
+		.list()
+		.layer(0, new ConvolutionLayer.Builder(5, 5)
+				.nIn(1)
+				.stride(2, 2)
+				.nOut(5)
+				.padding(1,1)
+				.dropOut(0.5)
+				.activation("relu")
+				.build())
+		.layer(1,
+				new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+				.kernelSize(2, 2)
+				.stride(2, 2)
+				.build())
+		.layer(2, new ConvolutionLayer.Builder(3, 3)
+				.nIn(1)
+				.stride(2, 2)
+				.nOut(5)
+				.padding(1,1)
+				.dropOut(0.5)
+				.activation("relu")
+				.build())
+		.layer(3,
+				new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+				.kernelSize(2, 2)
+				.stride(2, 2)
+				.build())
+		.layer(6, new DenseLayer.Builder()
+				.nOut(200)
+				.activation("sigmoid")
+				.build())
+		.layer(7, new OutputLayer.Builder(LossFunctions.LossFunction.SQUARED_LOSS)
+				.nOut(featureCount)
+				.activation("identity")
+				.build())
+		.backprop(true).pretrain(false);
+		new ConvolutionLayerSetup(builder, height, width, 3);
+		return builder;
+	}
 }
