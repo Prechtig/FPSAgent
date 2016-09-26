e693f813fda3b9d3077e742b26f0d5b5ff1c6f06
Mark <math@schantz.com>
2016-09-26 09:53:18 +0200

    Adds example code to testsaver

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/TestSaver.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/TestSaver.java
@@ -1,36 +1,16 @@
 package org.mma.imagerecognition.configuration;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.cpu.nativecpu.NDArray;
 
 public class TestSaver {
 	public static void main(String[] args) throws IOException {
-		
-//		File tempFile = new File(FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "MNISTTest/bestModel.bin"));
-//		FileInputStream fis = new FileInputStream(tempFile);
-//		MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(fis);
-//		
-//		INDArray ndArray = INDArrayTool.generateRandomVector(28*28);
-//		INDArray output = network.output(ndArray);
-//
-//		System.out.println(Arrays.toString(INDArrayTool.toFlatDoubleArray(output)));
-//		
-//		
-//		TrainingDbDao.initializeConnection();
-		
-		
-//		File model = new File("models\\bestModel.bin");
-//		FileInputStream fis = new FileInputStream(model);
-//		MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(fis);
-//		
-//		TrainingData td = TrainingDbDao.getRandomImages(1).get(0);
-//		INDArray indArray = ImageTool.convertToINDArray(td.getPixelData(), td.getWidth());
-//		INDArray output = network.output(indArray);
-//		
-//		System.out.println(Arrays.toString(INDArrayTool.toFlatDoubleArray(output)));
-//		System.out.println(Arrays.toString(td.getFeatureDoubles()));
-//		ImageTool.printPngImage(td.getPixelData(), td.getWidth(), new File("testimagefromnetwork.png"));
-		
-		
-		
+		MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(new FileInputStream(new File("models\\bestModel.bin")));
+		network.output(new NDArray());
 	}
 }
