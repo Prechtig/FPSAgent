3c9fed7507c94529a23605ffcffd94d66d8a835a
Mark <math@schantz.com>
2016-10-04 14:10:10 +0200

    Modifies scaling of distance

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/tools/ScaleTool.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/tools/ScaleTool.java
@@ -3,14 +3,13 @@ package org.mma.imagerecognition.tools;
 public class ScaleTool {
 	private static final double ANGLE_OFFSET = 90;
 	private static final double ANGLE_SCALE_FACTOR = 180;
-	private static final double MAX_DISTANCE = 50;
+	private static final double DISTANCE_SCALE_FACTOR = 0.1;
 	
 	public static double scaleAngle(double angle) {
 		return (angle + ANGLE_OFFSET) / ANGLE_SCALE_FACTOR;
 	}
 	
 	public static double scaleDistance(double distance) {
-		if (distance > MAX_DISTANCE) distance = MAX_DISTANCE;
-		return distance/MAX_DISTANCE;
+		return distance*DISTANCE_SCALE_FACTOR;
 	}
 }
