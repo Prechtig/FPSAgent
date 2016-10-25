aa107ccaa992c674ed2697d11fd5f09bf6c1168c
Mark <markthorh@gmail.com>
2016-10-25 11:33:11 +0200

    Adds continuousTraining class from previous commit, as of erroneous use of git theirs and ours

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/ContinuousTraining.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/ContinuousTraining.java
@@ -1,6 +1,7 @@
 package org.mma.imagerecognition.configuration;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -9,6 +10,7 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.parallelism.ParallelWrapper;
 import org.deeplearning4j.util.ModelSerializer;
+import org.mma.imagerecognition.dao.FileSystemDAO;
 import org.mma.imagerecognition.dataobjects.TrainingData;
 import org.mma.imagerecognition.executables.DeadNeuronDetector;
 import org.mma.imagerecognition.tools.PropertiesReader;
@@ -16,41 +18,32 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 public class ContinuousTraining implements Trainable {
 	
-	private String modelFilePath, modelFileName;
+	private MultiLayerNetwork model;
+	private MultiLayerConfiguration configuration;
+	private String modelFilePath;
 	private int height, width, featureCount, nEpochs;
+	private int latestEpoch = 0;
 	
-	public ContinuousTraining() {
+	public ContinuousTraining() throws FileNotFoundException, IOException {
 		init();
 	}
 
 	@Override
 	public void train(DataSetIterator trainIterator, DataSetIterator testIterator) {
-		MultiLayerConfiguration configuration = BuilderFactory.getShallowConvNet(height, width, featureCount).build();
-        MultiLayerNetwork model = new MultiLayerNetwork(configuration);
-        model.init();
-        
-        model.setListeners(new ScoreIterationListener(1));
-        
-        for(int i = 1; i <= nEpochs; i++) {
+        for(int i = latestEpoch+1; i <= nEpochs; i++) {
             model.fit(trainIterator);
-            System.out.println(String.format("*** Completed epoch %d ***", i));
-            DeadNeuronDetector.getDeadNeurons(model, 100);
+            System.out.println("*** Completed epoch {} ***" + i);
             try {
-				ModelSerializer.writeModel(model, modelFilePath + modelFileName + i +".bin", true);
+				ModelSerializer.writeModel(model, FileSystemDAO.getPathOfLatestModelFile(i).toString(), true);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+            DeadNeuronDetector.getDeadNeurons(model, 100);
             testIterator.reset();
         }
 	}
 	
 	public void trainParallel(DataSetIterator trainIterator, DataSetIterator testIterator) {
-		MultiLayerConfiguration configuration = BuilderFactory.getDeepConvNet(height, width, featureCount).build();
-        MultiLayerNetwork model = new MultiLayerNetwork(configuration);
-        model.init();
-        
-        model.setListeners(new ScoreIterationListener(1));
-		
 		ParallelWrapper wrapper = new ParallelWrapper.Builder(model)
 	        .prefetchBuffer(4)
 	        .workers(4)
@@ -59,12 +52,12 @@ public class ContinuousTraining implements Trainable {
 	        .useLegacyAveraging(true)
 	        .build();
 		
-		for(int i = 1; i <= nEpochs; i++) {
+		for(int i = latestEpoch; i <= nEpochs; i++) {
             wrapper.fit(trainIterator);
-            System.out.println(String.format("*** Completed epoch %d ***", i));
+            System.out.println("*** Completed epoch {} ***" + i);
             DeadNeuronDetector.getDeadNeurons(model, 100);
             try {
-				ModelSerializer.writeModel(model, modelFilePath + modelFileName + i +".bin", true);
+				ModelSerializer.writeModel(model, FileSystemDAO.getPathOfLatestModelFile(i).toString(), true);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -72,13 +65,28 @@ public class ContinuousTraining implements Trainable {
         }
 	}
 	
-	private void init() {
+	private void init() throws FileNotFoundException, IOException {
 		Properties projectProperties = PropertiesReader.getProjectProperties();
-		modelFilePath = projectProperties.getProperty("training.persistence.savePath") + File.separator + "continuous" + File.separator;
-		modelFileName = "model";
 		width = Integer.parseInt(projectProperties.getProperty("training.image.width"));
 		height = Integer.parseInt(projectProperties.getProperty("training.image.height"));
 		featureCount = TrainingData.getFeatureCount();
         nEpochs = Integer.parseInt(projectProperties.getProperty("training.epochs"));
+        initConfig();
+        initNetwork();
+	}
+	
+	private void initConfig() {
+		configuration = BuilderFactory.getDeepConvNet(height, width, featureCount).build();
+	}
+	
+	private void initNetwork() throws FileNotFoundException, IOException {
+		if(PropertiesReader.getProjectProperties().getProperty("training.continueFromLatestModel").equals("true")) {
+			model = ModelSerializer.restoreMultiLayerNetwork(new FileInputStream(FileSystemDAO.getPathOfLatestModelFile().toString()));
+			latestEpoch = FileSystemDAO.findLatestModelId();
+		} else {
+			model = new MultiLayerNetwork(configuration);
+	        model.init();
+		}
+		model.setListeners(new ScoreIterationListener(1));
 	}
 }
