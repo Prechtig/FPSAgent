89bc3c698e4e021f51c873cb5d576318dcfd6bfb
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2016-10-27 13:27:02 +0200

    Adds feature to save images to folders based on the presence of an enemy

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/PersistImages.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/PersistImages.java
@@ -2,30 +2,55 @@ package org.mma.imagerecognition.executables;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.mma.imagerecognition.dao.FileSystemDAO;
+import org.mma.imagerecognition.dao.TrainingDbDao;
 import org.mma.imagerecognition.dataobjects.TrainingData;
 import org.mma.imagerecognition.tools.ImageTool;
 
 public class PersistImages {
 	
+	private static final String imagesFolder = "images";
+	private static final String imagesWithBotsFolder = "images-with-bots";
+	private static final String imagesWithoutBotsFolder = "images-without-bots";
+	
 	public static void main(String[] args) throws IOException {
-		saveImages(250);
+		int upToId = 250;
+		
+		int maxPersistedId = FileSystemDAO.findLatestTrainingDataId();
+		
+		if(maxPersistedId < upToId) {
+			List<TrainingData> trainingData = TrainingDbDao.getTrainingData(1, upToId);
+			FileSystemDAO.createFolders();
+			FileSystemDAO.persist(trainingData);
+		}
+		
+		createFolders();
+		
+		saveImages(upToId);
 	}
 	
 	public static void saveImages(int upToId) throws IOException {
-		int maxIdToPersist = Math.min(FileSystemDAO.findLatestTrainingDataId(), upToId);
+		System.out.println(String.format("Persisting % 5d images", upToId));
 		
-		new File("images").mkdirs();
-		
-		System.out.println(String.format("Persisting  %d images", maxIdToPersist));
-		
-		for(int i = 1; i <= maxIdToPersist; i++) {
+		for(int i = 1; i <= upToId; i++) {
 			TrainingData td = FileSystemDAO.load(i);
-			ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File("images" + File.separator + i + ".png"));
-			if(i % 100 == 0 || i == maxIdToPersist) {
-				System.out.println(String.format("Persisted % 5d images", i));
+			if(td.getFeatureDoubles()[3] == 1d) {
+				ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithBotsFolder + File.separator + td.getId() + ".png"));
+			} else {
+				ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithoutBotsFolder + File.separator + td.getId() + ".png"));
+			}
+			ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesFolder + File.separator + td.getId() + ".png"));
+			if(i % 100 == 0 || i == upToId) {
+				System.out.println(String.format("Persisted % 6d images", i));
 			}
 		}
+	}
+	
+	public static void createFolders() {
+		new File(imagesFolder).mkdirs();
+		new File(imagesWithBotsFolder).mkdirs();
+		new File(imagesWithoutBotsFolder).mkdirs();
 	}
 }
