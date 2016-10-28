5ad895e239b18c908c61c444f026f9c7f379d7b7
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2016-10-28 13:02:03 +0200

    Makes it possible to persist images starting from id

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/PersistImages.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/PersistImages.java
@@ -3,6 +3,7 @@ package org.mma.imagerecognition.executables;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.mma.imagerecognition.dao.FileSystemDAO;
 import org.mma.imagerecognition.dao.TrainingDbDao;
@@ -14,37 +15,48 @@ public class PersistImages {
 	private static final String imagesFolder = "images";
 	private static final String imagesWithBotsFolder = "images-with-bots";
 	private static final String imagesWithoutBotsFolder = "images-without-bots";
+	private static final int batchDownloadSize = 250;
 	
 	public static void main(String[] args) throws IOException {
-		int upToId = 250;
+		int samples = 500;
+		int fromId = 45000;
+		int toId = fromId + samples;
 		
-		int maxPersistedId = FileSystemDAO.findLatestTrainingDataId();
-		
-		if(maxPersistedId < upToId) {
-			List<TrainingData> trainingData = TrainingDbDao.getTrainingData(1, upToId);
-			FileSystemDAO.createFolders();
+		FileSystemDAO.createFolders();
+		for(int id = fromId; id <= toId; id += batchDownloadSize) {
+			int endId = Math.min(toId, id + batchDownloadSize - 1);
+			List<TrainingData> trainingData = TrainingDbDao.getTrainingData(id, endId);
 			FileSystemDAO.persist(trainingData);
 		}
 		
 		createFolders();
+		saveImages(fromId, toId);
+	}
+	
+	public static void saveImages(int fromId, int toId) {
+		System.out.println(String.format("Persisting % 5d images", toId - fromId + 1));
 		
-		saveImages(upToId);
+		IntStream.rangeClosed(fromId, toId).boxed().forEach(id -> saveImage(id));
 	}
 	
 	public static void saveImages(int upToId) throws IOException {
 		System.out.println(String.format("Persisting % 5d images", upToId));
 		
-		for(int i = 1; i <= upToId; i++) {
-			TrainingData td = FileSystemDAO.load(i);
-			if(td.getFeatureDoubles()[3] == 1d) {
-				ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithBotsFolder + File.separator + td.getId() + ".png"));
-			} else {
-				ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithoutBotsFolder + File.separator + td.getId() + ".png"));
-			}
-			ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesFolder + File.separator + td.getId() + ".png"));
-			if(i % 100 == 0 || i == upToId) {
-				System.out.println(String.format("Persisted % 6d images", i));
-			}
+		IntStream.rangeClosed(1, upToId).boxed().forEach(id -> saveImage(id));
+	}
+	
+	public static void saveImage(int id) {
+		try {
+		TrainingData td = FileSystemDAO.load(id);
+		if(td.getFeatureDoubles()[3] == 1d) {
+			ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithBotsFolder + File.separator + td.getId() + ".png"));
+		} else {
+			ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesWithoutBotsFolder + File.separator + td.getId() + ".png"));
+		}
+		ImageTool.printColoredPngImage(td.getPixelData(), td.getWidth(), new File(imagesFolder + File.separator + td.getId() + ".png"));
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(42);
 		}
 	}
 	
