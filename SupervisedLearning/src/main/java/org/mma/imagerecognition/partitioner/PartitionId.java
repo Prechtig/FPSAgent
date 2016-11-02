b406b44249979e67b4b210430a97fcf75786c4fd
Mark <markthorh@gmail.com>
2016-11-02 18:08:57 +0100

    Makes partitionid fields public

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/partitioner/PartitionId.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/partitioner/PartitionId.java
@@ -2,9 +2,9 @@ package org.mma.imagerecognition.partitioner;
 
 public class PartitionId
 {		
-    public int InceptionLevel;
-    public int X;
-    public int Y;
+    public final int InceptionLevel;
+    public final int X;
+    public final int Y;
 
     public PartitionId(int inceptionLevel, int x, int y)
     {
