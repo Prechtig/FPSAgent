49795ac73c53a8d6cd8da79d269cc2c6a7815386
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2016-11-04 19:02:58 +0100

    Adds batching to evaluator

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/Evaluator.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/Evaluator.java
@@ -18,6 +18,9 @@ import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 public class Evaluator {
+	
+	private static final int BATCH_SIZE = 250;
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		Trainer.init();
 		FileSystemDAO.createFolders();
@@ -30,28 +33,36 @@ public class Evaluator {
 		MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(new FileInputStream(modelPath.toFile()));
 		System.out.format("Evaluating model %s \n", modelPath.toString());
 		double accuracy = getAccuracy(network);
-		System.out.format("Hit rate: %f \n", accuracy);
+		System.out.format("Hit rate: %f%%\n", accuracy*100);
 	}
 	
 	public static double getAccuracy(MultiLayerNetwork network) {
-//		List<TrainingData> testSet = TrainingDbDao.getTrainingData(60001, 60050);
-		List<TrainingData> testSet = FileSystemDAO.load(Trainer.lastValidationIndex()+1, Trainer.lastTestIndex());
-		if(testSet.size() == 0) {
+		int firstId = Trainer.lastValidationIndex()+1;
+		int lastId = Trainer.lastTestIndex();
+		
+		if(lastId-firstId < 0) {
 			throw new IllegalArgumentException("Test set is empty");
 		}
+		
+		@SuppressWarnings("unused")
 		int hits = 0, misses = 0;
 		
-		for(TrainingData td : testSet) {
-			double[] groundTruths = td.getFeatures();
-			INDArray output = network.output(Nd4j.create(ImageTool.toScaledDoubles(td.getPixelData()), new int[] { 1, 3, td.getWidth(), td.getHeight()}), false);
-			double[] binaryVector = NNTool.toBinaryVector(INDArrayTool.toFlatDoubleArray(output));
-			if(ArrayUtils.isEquals(binaryVector, groundTruths)) {
-				hits++;
-			} else {
-				misses++;
+		List<TrainingData> testSet;
+		for(int fromId = firstId; firstId <= lastId; firstId += BATCH_SIZE) {
+			int toId = Math.min(fromId + BATCH_SIZE, lastId);
+			testSet = FileSystemDAO.load(fromId, toId);
+			for(TrainingData td : testSet) {
+				double[] groundTruths = td.getFeatures();
+				INDArray output = network.output(Nd4j.create(ImageTool.toScaledDoubles(td.getPixelData()), new int[] { 1, 3, td.getWidth(), td.getHeight()}), false);
+				double[] binaryVector = NNTool.toBinaryVector(INDArrayTool.toFlatDoubleArray(output));
+				if(ArrayUtils.isEquals(binaryVector, groundTruths)) {
+					hits++;
+				} else {
+					misses++;
+				}
 			}
 		}
 		
-		return ((double) hits) / ((double) testSet.size());
+		return ((double) hits) / ((double) lastId-firstId+1);
 	}
 }
