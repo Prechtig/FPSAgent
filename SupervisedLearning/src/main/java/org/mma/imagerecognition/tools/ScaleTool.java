32dcae091eef64c9c19703d7633360344246f275
Mark <math@schantz.com>
2016-12-02 14:22:31 +0100

    changed scale

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/tools/ScaleTool.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/tools/ScaleTool.java
@@ -4,9 +4,15 @@ public class ScaleTool {
 	private static final double ANGLE_OFFSET = 90;
 	private static final double ANGLE_SCALE_FACTOR = 180;
 	private static final double DISTANCE_SCALE_FACTOR = 0.05;
+	private static double FOV = 0.0;
+	
+	static {
+		FOV = Double.parseDouble(PropertiesReader.getProjectProperties().getProperty("topology.fov"));
+	}
 	
 	public static double scaleAngle(double angle) {
-		return (angle + ANGLE_OFFSET) / ANGLE_SCALE_FACTOR;
+		return angle / (FOV/2);
+//		return (angle + ANGLE_OFFSET) / ANGLE_SCALE_FACTOR;
 	}
 	
 	public static double scaleDistance(double distance) {
