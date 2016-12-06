861a7fd9119a352d4464638861ef056050d4b87b
Mikkel Funch <mikkel.funch@gmail.com>
2016-12-06 21:04:09 +0100

    Added new fitness information to log. Changed agents sensitivity

--- Game/Assets/Scenes/ArenaScripts/NEATArena.cs
+++ Game/Assets/Scenes/ArenaScripts/NEATArena.cs
@@ -2,75 +2,82 @@
 using System.Collections;
 
 
-public class NEATArena : MonoBehaviour{
-	private float y;
-	private float x;
-	private float z;
-	private float WallHeight;
-	private float WallThickness;
+public class NEATArena : MonoBehaviour
+{
+    private float y;
+    private float x;
+    private float z;
+    private float WallHeight;
+    private float WallThickness;
 
-	public Transform[] PlayerSpawnPoints;
-	public Transform[] BotSpawnPoints;
+    public Transform[] PlayerSpawnPoints;
+    public Transform[] BotSpawnPoints;
 
-	//private RandomHorizontalPlayerSpawn PlayerSpawn;
-	//private RandomHorizontalBotSpawn BotSpawn;
-	private RandomPlayerSpawn PlayerSpawn;
-	public RandomBotSpawn BotSpawn;
+    //private RandomHorizontalPlayerSpawn PlayerSpawn;
+    //private RandomHorizontalBotSpawn BotSpawn;
+    private RandomPlayerSpawn PlayerSpawn;
+    public RandomBotSpawn BotSpawn;
 
-	private IList ArenaObjects;
+    private IList ArenaObjects;
 
-	private static int yOffset;
-	private static Object yOffsetLock = new Object();
+    private static int yOffset;
+    private static Object yOffsetLock = new Object();
 
-	public GameObject WallPrefab;
-	public GameObject FloorPrefab;
+    public GameObject WallPrefab;
+    public GameObject FloorPrefab;
 
 
-	private float RunningFitness = 0f;
-	private int RunningFitnessCount = 0;
-	private NEATWeapon neatWeapon;
+    private float RunningFitness = 0f;
+    private int RunningFitnessCount = 0;
+    private NEATWeapon neatWeapon;
 
-	public void Init() {
-		y = GetAndIncrementYOffset () * 100;
-		x = 25;
-		z = 25;
-		WallHeight = 50;
-		WallThickness = 0.1f;
-		ArenaObjects = new ArrayList ();
+    public void Init()
+    {
+        y = GetAndIncrementYOffset() * 100;
+        x = 25;
+        z = 25;
+        WallHeight = 50;
+        WallThickness = 0.1f;
+        ArenaObjects = new ArrayList();
 
-		CreateArena (x, z, WallHeight, WallThickness);
-        
-		//CreateLight ();
+        CreateArena(x, z, WallHeight, WallThickness);
+
+        //CreateLight ();
         CreateCloseLights();
 
-        SetSpawnPoints (x, z);
+        SetSpawnPoints(x, z);
 
-		SpawnPlayer ();
-		SpawnEnemies ();
+        SpawnPlayer();
+        SpawnEnemies();
 
-		MoveEnemyWithinSight ();
-	}
+        MoveEnemyWithinSight();
+    }
 
-    private static float GetAndIncrementYOffset(){
-		lock(yOffsetLock){
-			return yOffset++;
-		}
-	}
+    private static float GetAndIncrementYOffset()
+    {
+        lock (yOffsetLock)
+        {
+            return yOffset++;
+        }
+    }
 
-	public static void ResetYOffset(){
-		yOffset = 0;
-	}
+    public static void ResetYOffset()
+    {
+        yOffset = 0;
+    }
 
-	void FixedUpdate () {
-		if (BotSpawn.Bots.Count > 0) {
-			float angle = float.MaxValue;
-			float k = 75f;
-			float c = 2f;
+    void FixedUpdate()
+    {
+        if (BotSpawn.Bots.Count > 0)
+        {
+            float angle = float.MaxValue;
+            float k = 75f;
+            float c = 2f;
 
-			RaycastHit hit;
-			Vector3 direction = neatWeapon.gameObject.transform.TransformDirection (0, 0, 1);
+            RaycastHit hit;
+            Vector3 direction = neatWeapon.gameObject.transform.TransformDirection(0, 0, 1);
 
-            
+
             foreach (GameObject bot in BotSpawn.Bots)
             {
                 float tempAngle = float.MaxValue;
@@ -88,173 +95,186 @@ public class NEATArena : MonoBehaviour{
                     angle = tempAngle;
                 }
             }
-			
 
-			//RunningFitness += k / (1 + (angle * c));
-			RunningFitness += k / Mathf.Pow((1 + angle), c);
-			RunningFitnessCount++;
-		}
-	}
 
-	private void CreateArena(float x, float z, float wallHeight, float wallThickness) {
-		// Create floor
-		CreateCube (x, 0.1f, z, 0, y, 0, FloorPrefab);
+            //RunningFitness += k / (1 + (angle * c));
+            RunningFitness += k / Mathf.Pow((1 + angle), c);
+            RunningFitnessCount++;
+        }
+    }
 
-		// Create northern wall
-		CreateCube (x, wallHeight, wallThickness, 0, wallHeight/2 + y, z/2);
+    private void CreateArena(float x, float z, float wallHeight, float wallThickness)
+    {
+        // Create floor
+        CreateCube(x, 0.1f, z, 0, y, 0, FloorPrefab);
 
-		// Create eastern wall
-		CreateCube (wallThickness, wallHeight, z, 0-(x/2), wallHeight/2 + y, 0);
+        // Create northern wall
+        CreateCube(x, wallHeight, wallThickness, 0, wallHeight / 2 + y, z / 2);
 
-		// Create western wall
-		CreateCube (wallThickness, wallHeight, z, x/2, wallHeight/2 + y, 0);
+        // Create eastern wall
+        CreateCube(wallThickness, wallHeight, z, 0 - (x / 2), wallHeight / 2 + y, 0);
 
-		// Create southern wall
-		CreateCube (x, wallHeight, wallThickness, 0, wallHeight/2 + y, 0-(z/2));
-	}
+        // Create western wall
+        CreateCube(wallThickness, wallHeight, z, x / 2, wallHeight / 2 + y, 0);
 
-	private void CreateCube(float sx, float sy, float sz, float px, float py, float pz) {
-		CreateCube (sx, sy, sz, px, py, pz, WallPrefab);
-	}
+        // Create southern wall
+        CreateCube(x, wallHeight, wallThickness, 0, wallHeight / 2 + y, 0 - (z / 2));
+    }
 
-	private void CreateCube(float sx, float sy, float sz, float px, float py, float pz, GameObject texturePrefab) {
-		Vector3 scale = new Vector3 (sx, sy, sz);
-		Vector3 position = new Vector3 (px, py, pz);
-		CreateCube (position, scale, texturePrefab);
-	}
+    private void CreateCube(float sx, float sy, float sz, float px, float py, float pz)
+    {
+        CreateCube(sx, sy, sz, px, py, pz, WallPrefab);
+    }
 
-	private void CreateCube(Vector3 position, Vector3 scale) {
-		CreateCube (position, scale, WallPrefab);
-	}
+    private void CreateCube(float sx, float sy, float sz, float px, float py, float pz, GameObject texturePrefab)
+    {
+        Vector3 scale = new Vector3(sx, sy, sz);
+        Vector3 position = new Vector3(px, py, pz);
+        CreateCube(position, scale, texturePrefab);
+    }
 
-	private void CreateCube(Vector3 position, Vector3 scale, GameObject texturePrefab) {
-		GameObject cube = Instantiate(texturePrefab);
-		cube.transform.position = position;
-		cube.transform.localScale = scale;
+    private void CreateCube(Vector3 position, Vector3 scale)
+    {
+        CreateCube(position, scale, WallPrefab);
+    }
 
-		ArenaObjects.Add (cube);
-	}
+    private void CreateCube(Vector3 position, Vector3 scale, GameObject texturePrefab)
+    {
+        GameObject cube = Instantiate(texturePrefab);
+        cube.transform.position = position;
+        cube.transform.localScale = scale;
 
-	private void CreateLight(){
-		Vector3 position = new Vector3 (0, 15 + y, 0);
-		GameObject obj = new GameObject ();
-		obj.transform.position = position;
-		Light l = obj.AddComponent<Light> ();
-		l.type = LightType.Point;
-		l.color = new Color (255/255.0f, 244/255.0f, 214/255.0f);
-		l.shadows = LightShadows.None;
-		l.range = 75;
-		l.intensity = 2;
-		obj.name = "Light";
+        ArenaObjects.Add(cube);
+    }
 
-		ArenaObjects.Add (obj);
+    private void CreateLight()
+    {
+        Vector3 position = new Vector3(0, 15 + y, 0);
+        GameObject obj = new GameObject();
+        obj.transform.position = position;
+        Light l = obj.AddComponent<Light>();
+        l.type = LightType.Point;
+        l.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
+        l.shadows = LightShadows.None;
+        l.range = 75;
+        l.intensity = 2;
+        obj.name = "Light";
+
+        ArenaObjects.Add(obj);
     }
 
     private void CreateCloseLights()
     {
-		Vector3 rotation = new Vector3 (90, 0, 0);
+        Vector3 rotation = new Vector3(90, 0, 0);
 
         GameObject obj = new GameObject();
-		Vector3 position = new Vector3(-((x/2)/2), 20 + y, z / 2 - 1.8f);
+        Vector3 position = new Vector3(-((x / 2) / 2), 20 + y, z / 2 - 1.8f);
         obj.transform.position = position;
-		obj.transform.eulerAngles = rotation;
-		Light light = obj.AddComponent<Light>();
-		light.shadows = LightShadows.None;
+        obj.transform.eulerAngles = rotation;
+        Light light = obj.AddComponent<Light>();
+        light.shadows = LightShadows.None;
         light.type = LightType.Spot;
         light.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
-		light.range = 100;
-		light.spotAngle = 90;
-		light.intensity = 3;
+        light.range = 100;
+        light.spotAngle = 90;
+        light.intensity = 3;
         obj.name = "Left Light";
         ArenaObjects.Add(obj);
 
-		obj = new GameObject();
-		position = new Vector3 ((z / 2) - 1.5f, 20 + y, (z / 2) - 1.5f);
-		obj.transform.position = position;
-		obj.transform.eulerAngles = rotation;
-		light = obj.AddComponent<Light>();
-		light.shadows = LightShadows.None;
-		light.type = LightType.Spot;
-		light.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
-		light.range = 100;
-		light.spotAngle = 179;
-		light.intensity = 1;
-		obj.name = "Right Light";
-		ArenaObjects.Add(obj);
+        obj = new GameObject();
+        position = new Vector3((z / 2) - 1.5f, 20 + y, (z / 2) - 1.5f);
+        obj.transform.position = position;
+        obj.transform.eulerAngles = rotation;
+        light = obj.AddComponent<Light>();
+        light.shadows = LightShadows.None;
+        light.type = LightType.Spot;
+        light.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
+        light.range = 100;
+        light.spotAngle = 179;
+        light.intensity = 1;
+        obj.name = "Right Light";
+        ArenaObjects.Add(obj);
 
-		obj = new GameObject();
-		position = new Vector3(0, 20 + y, (-z/2) + 2.5f);
-		obj.transform.position = position;
-		obj.transform.eulerAngles = rotation;
-		light = obj.AddComponent<Light>();
-		light.shadows = LightShadows.None;
-		light.type = LightType.Spot;
-		light.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
-		light.range = 90;
-		light.spotAngle = 90;
-		light.intensity = 1;
-		obj.name = "Player Light";
-		ArenaObjects.Add(obj);
+        obj = new GameObject();
+        position = new Vector3(0, 20 + y, (-z / 2) + 2.5f);
+        obj.transform.position = position;
+        obj.transform.eulerAngles = rotation;
+        light = obj.AddComponent<Light>();
+        light.shadows = LightShadows.None;
+        light.type = LightType.Spot;
+        light.color = new Color(255 / 255.0f, 244 / 255.0f, 214 / 255.0f);
+        light.range = 90;
+        light.spotAngle = 90;
+        light.intensity = 1;
+        obj.name = "Player Light";
+        ArenaObjects.Add(obj);
     }
 
-    private void SetSpawnPoints(float x, float z){
-		PlayerSpawnPoints = new Transform[1];
-		GameObject tempSpawnObject = (new GameObject("SpawnObject"));
-		Transform a = tempSpawnObject.transform;
-		a.Translate(new Vector3(0,1 + y,0));
-		PlayerSpawnPoints[0] = a;
+    private void SetSpawnPoints(float x, float z)
+    {
+        PlayerSpawnPoints = new Transform[1];
+        GameObject tempSpawnObject = (new GameObject("SpawnObject"));
+        Transform a = tempSpawnObject.transform;
+        a.Translate(new Vector3(0, 1 + y, 0));
+        PlayerSpawnPoints[0] = a;
 
-		BotSpawnPoints = new Transform[1];
-		tempSpawnObject = (new GameObject("SpawnObject"));
-		BotSpawnPoints[0] = tempSpawnObject.transform;
-		//tempSpawnObject = (new GameObject("SpawnObject"));
-		//BotSpawnPoints[1] = tempSpawnObject.transform;
+        BotSpawnPoints = new Transform[1];
+        tempSpawnObject = (new GameObject("SpawnObject"));
+        BotSpawnPoints[0] = tempSpawnObject.transform;
+        //tempSpawnObject = (new GameObject("SpawnObject"));
+        //BotSpawnPoints[1] = tempSpawnObject.transform;
 
 
-		Vector3 vBot0 = new Vector3 (x / 2 - 2, 1 + y, z / 2 - 2);
-		BotSpawnPoints [0].Translate (vBot0);
-		//Vector3 vBot1 = new Vector3 (-(x / 2) + 2, 1 + y, z / 2 - 2);
-		//BotSpawnPoints [1].Translate (vBot1);
+        Vector3 vBot0 = new Vector3(x / 2 - 2, 1 + y, z / 2 - 2);
+        BotSpawnPoints[0].Translate(vBot0);
+        //Vector3 vBot1 = new Vector3 (-(x / 2) + 2, 1 + y, z / 2 - 2);
+        //BotSpawnPoints [1].Translate (vBot1);
 
-		//BotSpawnPoints [0].LookAt (vBot1);
-		//BotSpawnPoints [1].LookAt (vBot0);
-	}
+        //BotSpawnPoints [0].LookAt (vBot1);
+        //BotSpawnPoints [1].LookAt (vBot0);
+    }
 
-	public GameObject GetPlayer(){
-		return PlayerSpawn.Player;
-	}
+    public GameObject GetPlayer()
+    {
+        return PlayerSpawn.Player;
+    }
 
-	public void SpawnPlayer(){
-		PlayerSpawn = gameObject.AddComponent<RandomPlayerSpawn>();
-		PlayerSpawn.X = x;
-		PlayerSpawn.Z = z;
-		PlayerSpawn.SpawnPoints = PlayerSpawnPoints;
-		PlayerSpawn.Arena = this;
-		PlayerSpawn.SpawnPlayer ();
-		neatWeapon = PlayerSpawn.Player.GetComponentInChildren<NEATWeapon> ();
-	}
+    public void SpawnPlayer()
+    {
+        PlayerSpawn = gameObject.AddComponent<RandomPlayerSpawn>();
+        PlayerSpawn.X = x;
+        PlayerSpawn.Z = z;
+        PlayerSpawn.SpawnPoints = PlayerSpawnPoints;
+        PlayerSpawn.Arena = this;
+        PlayerSpawn.SpawnPlayer();
+        neatWeapon = PlayerSpawn.Player.GetComponentInChildren<NEATWeapon>();
+    }
 
-	public void SpawnEnemies(){
-		BotSpawn = gameObject.AddComponent<RandomBotSpawn> ();
-		BotSpawn.X = x;
-		BotSpawn.Z = z;
-		BotSpawn.SpawnPoints = BotSpawnPoints;
+    public void SpawnEnemies()
+    {
+        BotSpawn = gameObject.AddComponent<RandomBotSpawn>();
+        BotSpawn.X = x;
+        BotSpawn.Z = z;
+        BotSpawn.SpawnPoints = BotSpawnPoints;
         BotSpawn.Player = PlayerSpawn.Player;
 
-        BotSpawn.StartSpawning ();
+        BotSpawn.StartSpawning();
     }
 
-	private void MoveEnemyWithinSight(){
-		Camera cam = GetPlayer ().GetComponentInChildren<Camera> ();
-		while(!GameObjectHelper.IsObjectWithinSight(cam, BotSpawn.Bots[0])){
-			BotSpawn.Bots [0].transform.position = BotSpawn.GenerateSpawnPoint ();
-		}
-		//GetPlayer ().transform.LookAt (BotSpawn.Bots [0].transform);
-		//GetPlayer ().transform.eulerAngles = new Vector3(PlayerSpawn.Player.transform.eulerAngles.x + 4, PlayerSpawn.Player.transform.eulerAngles.y);
-	}
+    private void MoveEnemyWithinSight()
+    {
+        Camera cam = GetPlayer().GetComponentInChildren<Camera>();
+        while (!GameObjectHelper.IsObjectWithinSight(cam, BotSpawn.Bots[0]))
+        {
+            BotSpawn.Bots[0].transform.position = BotSpawn.GenerateSpawnPoint();
+        }
+        //GetPlayer ().transform.LookAt (BotSpawn.Bots [0].transform);
+        //GetPlayer ().transform.eulerAngles = new Vector3(PlayerSpawn.Player.transform.eulerAngles.x + 4, PlayerSpawn.Player.transform.eulerAngles.y);
+    }
 
-	public float GetFitness(){
-		float fitness = 0f;
+    public Pair<float, float> GetFitness()
+    {
+        float fitness = 0f;
 
         //New fitness function, does not work with the HorizontalBotSpawn
         /*
@@ -269,30 +289,60 @@ public class NEATArena : MonoBehaviour{
 		fitness = k / (1 + (angle * c));
 		*/
         //GetPlayer().GetComponent<NEATController>().GetFitness();
-		fitness += RunningFitness / RunningFitnessCount;
-		return fitness + BotSpawn.GetFitness ();
-	}
 
-	public void OnDestroy(){
-		if (ArenaObjects != null) {
-			foreach (GameObject o in ArenaObjects) {
-				if (o != null) {
-					Destroy (o.gameObject);
-				}
-			}
-		}
+        fitness += RunningFitness / RunningFitnessCount;
+        fitness += BotSpawn.GetFitness();
+        Pair<float, float> p = new Pair<float, float>(fitness, RunningFitness / RunningFitnessCount);
 
-		foreach (Transform t in PlayerSpawnPoints) {
-			if (t != null) {
-				Destroy (t.gameObject);
-			}
-		}
-		foreach (Transform t in BotSpawnPoints) {
-			if (t != null) {
-				Destroy (t.gameObject);
-			}
-		}
-		Destroy (BotSpawn);
-		Destroy (PlayerSpawn);
-	}
+        return p;
+    }
+
+    public void OnDestroy()
+    {
+        if (ArenaObjects != null)
+        {
+            foreach (GameObject o in ArenaObjects)
+            {
+                if (o != null)
+                {
+                    Destroy(o.gameObject);
+                }
+            }
+        }
+
+        foreach (Transform t in PlayerSpawnPoints)
+        {
+            if (t != null)
+            {
+                Destroy(t.gameObject);
+            }
+        }
+        foreach (Transform t in BotSpawnPoints)
+        {
+            if (t != null)
+            {
+                Destroy(t.gameObject);
+            }
+        }
+        Destroy(BotSpawn);
+        Destroy(PlayerSpawn);
+    }
+
+
+}
+
+public class Pair<T, U>
+{
+    public Pair()
+    {
+    }
+
+    public Pair(T first, U second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+
+    public T First { get; set; }
+    public U Second { get; set; }
 }
