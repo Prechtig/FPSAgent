2046e7a70bd1ac6283dd430d62b7d645d3d9632b
Mikkel Funch <mikkel.funch@gmail.com>
2016-12-09 14:40:24 +0100

    Remove start animation

--- Game/Assets/Scripts/Weapons/NEATWeapon.cs
+++ Game/Assets/Scripts/Weapons/NEATWeapon.cs
@@ -4,147 +4,148 @@ using System.Collections;
 
 public class NEATWeapon : MonoBehaviour
 {
-	public Animation anim;
-	public AnimationClip fireAnim;
-	public AnimationClip reloadAnim;
-	public AnimationClip reloadEmptyAnim;
-	public AnimationClip drawAnim;
+    public Animation anim;
+    public AnimationClip fireAnim;
+    public AnimationClip reloadAnim;
+    public AnimationClip reloadEmptyAnim;
+    public AnimationClip drawAnim;
 
-	#region bools
+    #region bools
 
-	public bool reloading;
-	//public bool[] canAims;
-	//private bool canAim;
-	//public bool[] canReloads;
-	//private bool canReload;
-	//public bool[] canFires;
-	private bool canFire;
+    public bool reloading;
+    //public bool[] canAims;
+    //private bool canAim;
+    //public bool[] canReloads;
+    //private bool canReload;
+    //public bool[] canFires;
+    private bool canFire;
 
-	#endregion
+    #endregion
 
-	#region stats
+    #region stats
 
-	public float fireRate = 0.1f;
-	public float timer = 0;
-	[SerializeField]
-	protected int bulletsLeft = 30;
-	[SerializeField]
-	protected int bulletsPerMag = 30;
-	[SerializeField]
-	protected int magsLeft = 4;
-	public float range = 2000;
-	public float damageMin = 10;
-	public float damageMax = 20;
-	public Transform bulletGo;
-	public LayerMask hitLayers;
-	public GameObject blood;
-	public GameObject concrete;
-	public GameObject wood;
-	public GameObject metal;
-	public GameObject dirt;
+    public float fireRate = 0.1f;
+    public float timer = 0;
+    [SerializeField]
+    protected int bulletsLeft = 30;
+    [SerializeField]
+    protected int bulletsPerMag = 30;
+    [SerializeField]
+    protected int magsLeft = 4;
+    public float range = 2000;
+    public float damageMin = 10;
+    public float damageMax = 20;
+    public Transform bulletGo;
+    public LayerMask hitLayers;
+    public GameObject blood;
+    public GameObject concrete;
+    public GameObject wood;
+    public GameObject metal;
+    public GameObject dirt;
 
-	#endregion
+    #endregion
 
-	#region readOnly
+    #region readOnly
 
-	public int bulletsLeftRead = 30;
-	public int bulletsPerMagRead = 30;
-	public int magsLeftRead = 10;
+    public int bulletsLeftRead = 30;
+    public int bulletsPerMagRead = 30;
+    public int magsLeftRead = 10;
 
-	#endregion
+    #endregion
 
 
-	#region components
+    #region components
 
-	public CharacterValues cv;
-	public PlayerAnimations pa;
+    public CharacterValues cv;
+    public PlayerAnimations pa;
 
-	#endregion
+    #endregion
 
-	#region ads
+    #region ads
 
-	public Camera cam;
-	public bool aiming;
-	public float hipFov = 75;
-	public float aimFov = 55;
-	private float curFov = 75;
-	public Vector3 hipPos;
-	public Vector3 crouchPos;
-	public Vector3 aimPos;
-	private Vector3 curPos;
+    public Camera cam;
+    public bool aiming;
+    public float hipFov = 75;
+    public float aimFov = 55;
+    private float curFov = 75;
+    public Vector3 hipPos;
+    public Vector3 crouchPos;
+    public Vector3 aimPos;
+    private Vector3 curPos;
 
-	#endregion
+    #endregion
 
-	#region recoil
+    #region recoil
 
-	public Transform camKB;
-	public Transform wepKB;
-	public float minKB;
-	public float maxKB;
-	public float minKBSide;
-	public float maxKBSide;
-	public float returnSpeed = 5f;
+    public Transform camKB;
+    public Transform wepKB;
+    public float minKB;
+    public float maxKB;
+    public float minKBSide;
+    public float maxKBSide;
+    public float returnSpeed = 5f;
 
-	#endregion
+    #endregion
 
-	#region muzzle
+    #region muzzle
 
-	public GameObject muzzle;
+    public GameObject muzzle;
 
-	#endregion
+    #endregion
 
-	#region crosshair
+    #region crosshair
 
-	public float sizeMultiplier = 1f;
-	public float aimSpread;
-	public float basicSpread = 30;
-	public float maximumSpread = 100;
-	public float spreadReturnTime = 5;
-	public float spreadAddPerShot = 5;
-	public float spreadTemp;
-	private float spread = 30;
+    public float sizeMultiplier = 1f;
+    public float aimSpread;
+    public float basicSpread = 30;
+    public float maximumSpread = 100;
+    public float spreadReturnTime = 5;
+    public float spreadAddPerShot = 5;
+    public float spreadTemp;
+    private float spread = 30;
 
-	//Crosshair Textures
-	public Texture2D crosshairFirstModeHorizontal;
-	public Texture2D crosshairFirstModeVertical;
+    //Crosshair Textures
+    public Texture2D crosshairFirstModeHorizontal;
+    public Texture2D crosshairFirstModeVertical;
 
-	#endregion
+    #endregion
 
-	#region private
+    #region private
 
-	private Vector2 pivot;
+    private Vector2 pivot;
 
-	#endregion
+    #endregion
 
-	#region hitMark
+    #region hitMark
 
-	public Texture2D tex;
-	public float size = 32;
-	//private float hitAlpha;
-	//public AudioClip hitMarkerSound;
+    public Texture2D tex;
+    public float size = 32;
+    //private float hitAlpha;
+    //public AudioClip hitMarkerSound;
 
-	#endregion
-    
-    
-	public Text ShotsLeftText;
-	private System.Random rng;
+    #endregion
+
+
+    public Text ShotsLeftText;
+    private System.Random rng;
 
     public int WrongReloads;
     public int Shots;
     public int Misses;
 
-	void Start ()
-	{
-		muzzle.SetActive (false);
-		//spreadTemp = basicSpread;
-		//spread = basicSpread;
-		//StartCoroutine (CheckBools ());
-		StartCoroutine (Draw ());
-		//shotsLeftText = GameObject.FindWithTag("UIText").GetComponent<Text>() as Text;
-	}
+    void Start()
+    {
+        muzzle.SetActive(false);
+        //spreadTemp = basicSpread;
+        //spread = basicSpread;
+        //StartCoroutine (CheckBools ());
+        canFire = true;
+        //StartCoroutine (Draw ());
+        //shotsLeftText = GameObject.FindWithTag("UIText").GetComponent<Text>() as Text;
+    }
 
-	void FixedUpdate ()
-	{
+    void FixedUpdate()
+    {
         /*if (hitAlpha > 0)
 			hitAlpha -= Time.deltaTime;
 		
@@ -152,10 +153,10 @@ public class NEATWeapon : MonoBehaviour
 			spread = aimSpread;*/
         spread = Mathf.Clamp(spread, 0, maximumSpread);
         //Debug.Log("First: " + spread);
-        spread = Mathf.Lerp (spread, spreadTemp + cv.velMag * 2, Time.fixedDeltaTime * 8);
+        spread = Mathf.Lerp(spread, spreadTemp + cv.velMag * 2, Time.fixedDeltaTime * 8);
         //Debug.Log("Secon: " + spread);
         if (spreadTemp > basicSpread)
-			spreadTemp -= Time.fixedDeltaTime * spreadReturnTime;
+            spreadTemp -= Time.fixedDeltaTime * spreadReturnTime;
         if (spreadTemp < 0)
             spreadTemp = 0;
         //Debug.Log("SpreadTemp: " + spread);
@@ -188,9 +189,9 @@ public class NEATWeapon : MonoBehaviour
 			aiming = false;
 		 */
         ShotsLeftText.text = bulletsLeft + " / " + magsLeft;
-	}
+    }
 
-	/*
+    /*
 	void OnGUI ()
 	{
 		float w = crosshairFirstModeHorizontal.width;
@@ -202,7 +203,7 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	/*
+    /*
 	void CheckInput ()
 	{
 
@@ -218,47 +219,51 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	public float GetRandomFloat(double min, double max){
-		if (spreadTemp == 0) {
-			rng = new System.Random (1337);
-		}
-		/*
+    public float GetRandomFloat(double min, double max)
+    {
+        if (spreadTemp == 0)
+        {
+            rng = new System.Random(1337);
+        }
+        /*
 		double mantissa = (rng.NextDouble() * 2.0) - 1.0;
 		double exponent = System.Math.Pow(2.0, rng.Next(-126, 128));
 		return (float)(mantissa * exponent);
 		*/
-		return (float)(min + (rng.NextDouble() * (max - min)));
-	}
+        return (float)(min + (rng.NextDouble() * (max - min)));
+    }
 
-	public void FireOneShot ()
-	{
-		if (!reloading && Time.time > timer && canFire && bulletsLeft > 0) {// && Screen.lockCursor)
-			//spreadTemp += spreadAddPerShot;
-			timer = Time.time + fireRate;
-			anim.Rewind (fireAnim.name);
-			anim.Play (fireAnim.name);
-			//localSource.clip = fireSound;
-			//localSource.PlayOneShot (fireSound);
-			StartCoroutine (MuzzleFlash ());
+    public void FireOneShot()
+    {
+        if (!reloading && Time.time > timer && canFire && bulletsLeft > 0)
+        {// && Screen.lockCursor)
+         //spreadTemp += spreadAddPerShot;
+            timer = Time.time + fireRate;
+            anim.Rewind(fireAnim.name);
+            anim.Play(fireAnim.name);
+            //localSource.clip = fireSound;
+            //localSource.PlayOneShot (fireSound);
+            StartCoroutine(MuzzleFlash());
 
-			//Weapon kick
-			//StartCoroutine(Kick3(camKB, new Vector3(-Random.Range(minKB, maxKB), Random.Range(minKBSide, maxKBSide), 0), 0.1f));
-			//StartCoroutine(Kick3(wepKB, new Vector3(-Random.Range(minKB, maxKB), Random.Range(minKBSide, maxKBSide), 0), 0.1f));
+            //Weapon kick
+            //StartCoroutine(Kick3(camKB, new Vector3(-Random.Range(minKB, maxKB), Random.Range(minKBSide, maxKBSide), 0), 0.1f));
+            //StartCoroutine(Kick3(wepKB, new Vector3(-Random.Range(minKB, maxKB), Random.Range(minKBSide, maxKBSide), 0), 0.1f));
 
-			//float actualSpread = Random.Range (-spread, spread);
-			//Vector3 position = new Vector3 (bulletGo.position.x - actualSpread, bulletGo.position.y - actualSpread, bulletGo.position.z);
+            //float actualSpread = Random.Range (-spread, spread);
+            //Vector3 position = new Vector3 (bulletGo.position.x - actualSpread, bulletGo.position.y - actualSpread, bulletGo.position.z);
 
-			//Bullet spread
-			//Vector3 direction = gameObject.transform.TransformDirection(new Vector3(Random.Range(-0.01f, 0.01f) * spread, Random.Range(0, 0.01f) * spread * 3, 1));
-			Vector3 direction = gameObject.transform.TransformDirection(new Vector3(GetRandomFloat(-0.01d, 0.01d) * spread, GetRandomFloat(0, 0.01d) * spread * 3, 1));
-			//Vector3 direction = gameObject.transform.TransformDirection (0, 0, 1);
+            //Bullet spread
+            //Vector3 direction = gameObject.transform.TransformDirection(new Vector3(Random.Range(-0.01f, 0.01f) * spread, Random.Range(0, 0.01f) * spread * 3, 1));
+            Vector3 direction = gameObject.transform.TransformDirection(new Vector3(GetRandomFloat(-0.01d, 0.01d) * spread, GetRandomFloat(0, 0.01d) * spread * 3, 1));
+            //Vector3 direction = gameObject.transform.TransformDirection (0, 0, 1);
 
 
-			RaycastHit hit2;
-			if (Physics.Raycast (bulletGo.position, direction, out hit2, range, hitLayers)) {
-				OnHit (hit2);
-			}
-			bulletsLeft--;
+            RaycastHit hit2;
+            if (Physics.Raycast(bulletGo.position, direction, out hit2, range, hitLayers))
+            {
+                OnHit(hit2);
+            }
+            bulletsLeft--;
             if (spreadTemp == 0)
             {
                 spreadTemp += spreadAddPerShot;
@@ -274,88 +279,102 @@ public class NEATWeapon : MonoBehaviour
             }
             //Debug.Log("Temp: " + spreadTemp);
         }
-	}
+    }
 
-	void DoHitMark ()
-	{
-		//hitAlpha = 2;
-		//GetComponent<AudioSource> ().PlayOneShot (hitMarkerSound, 1f);
-	}
+    void DoHitMark()
+    {
+        //hitAlpha = 2;
+        //GetComponent<AudioSource> ().PlayOneShot (hitMarkerSound, 1f);
+    }
 
-	void OnHit (RaycastHit hit)
-	{
+    void OnHit(RaycastHit hit)
+    {
         Shots++;
-		if (hit.transform.tag == "Bot") {
-			Instantiate (blood, hit.point, Quaternion.identity);
-			DoHitMark ();
-			if (hit.transform.GetComponent<BotVitals> ()) {
-				hit.transform.GetComponent<BotVitals> ().ApplyDamage (Random.Range (damageMin, damageMax), 1);
-			}
-		} else {
+        if (hit.transform.tag == "Bot")
+        {
+            Instantiate(blood, hit.point, Quaternion.identity);
+            DoHitMark();
+            if (hit.transform.GetComponent<BotVitals>())
+            {
+                hit.transform.GetComponent<BotVitals>().ApplyDamage(Random.Range(damageMin, damageMax), 1);
+            }
+        }
+        else
+        {
             Misses++;
-			if (hit.transform.tag == "Wood") {
-				GameObject theObj = Instantiate (wood, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation (Vector3.up, hit.normal)) as GameObject;
-				theObj.transform.parent = hit.transform;
-			} else if (hit.transform.tag == "Metal") {
-				GameObject theObj = Instantiate (metal, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation (Vector3.up, hit.normal)) as GameObject;
-				theObj.transform.parent = hit.transform;
-			} else if (hit.transform.tag == "Dirt") {
-				GameObject theObj = Instantiate (dirt, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation (Vector3.up, hit.normal)) as GameObject;
-				theObj.transform.parent = hit.transform;
-			} else {
-				GameObject theObj = Instantiate (concrete, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation (Vector3.up, hit.normal)) as GameObject;
-				theObj.transform.parent = hit.transform;
-			}
-		}
-	}
+            if (hit.transform.tag == "Wood")
+            {
+                GameObject theObj = Instantiate(wood, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+                theObj.transform.parent = hit.transform;
+            }
+            else if (hit.transform.tag == "Metal")
+            {
+                GameObject theObj = Instantiate(metal, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+                theObj.transform.parent = hit.transform;
+            }
+            else if (hit.transform.tag == "Dirt")
+            {
+                GameObject theObj = Instantiate(dirt, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+                theObj.transform.parent = hit.transform;
+            }
+            else
+            {
+                GameObject theObj = Instantiate(concrete, hit.point + hit.normal * 0.01f, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+                theObj.transform.parent = hit.transform;
+            }
+        }
+    }
 
-	IEnumerator Kick3 (Transform goTransform, Vector3 kbDirection, float time)
-	{
-		Quaternion startRotation = goTransform.localRotation;
-		Quaternion endRotation = goTransform.localRotation * Quaternion.Euler (kbDirection);
-		float rate = 1.0f / time;
-		var t = 0.0f;
-		while (t < 1.0f) {
-			t += Time.fixedDeltaTime * rate;
-			goTransform.localRotation = Quaternion.Slerp (startRotation, endRotation, t);
-			yield return null;
-		}
-	}
+    IEnumerator Kick3(Transform goTransform, Vector3 kbDirection, float time)
+    {
+        Quaternion startRotation = goTransform.localRotation;
+        Quaternion endRotation = goTransform.localRotation * Quaternion.Euler(kbDirection);
+        float rate = 1.0f / time;
+        var t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.fixedDeltaTime * rate;
+            goTransform.localRotation = Quaternion.Slerp(startRotation, endRotation, t);
+            yield return null;
+        }
+    }
 
-	public void Reload ()
-	{
-		if (!reloading && magsLeft > 0) {// && Screen.lockCursor)
-			reloading = true;
-			StartCoroutine (RunReload ());
-		}
-	}
+    public void Reload()
+    {
+        if (!reloading && magsLeft > 0)
+        {// && Screen.lockCursor)
+            reloading = true;
+            StartCoroutine(RunReload());
+        }
+    }
 
-	private IEnumerator RunReload(){
-		reloading = true;
-		//canAims [0] = false;
-		//canFires [0] = false;
-		//canReloads [0] = false;
-		anim [reloadAnim.name].speed = 3;
-		anim [reloadEmptyAnim.name].speed = 3;
+    private IEnumerator RunReload()
+    {
+        reloading = true;
+        //canAims [0] = false;
+        //canFires [0] = false;
+        //canReloads [0] = false;
+        anim[reloadAnim.name].speed = 3;
+        anim[reloadEmptyAnim.name].speed = 3;
 
         if (bulletsLeft == bulletsPerMag)
         {
             WrongReloads++;
         }
 
-		//StartCoroutine (ReloadingSound (reloadSoundsEmpty));
-		anim.Play (reloadEmptyAnim.name);
-		yield return new WaitForSeconds (reloadEmptyAnim.length / 3);
-		bulletsLeft = bulletsPerMag;
-		magsLeft--;
+        //StartCoroutine (ReloadingSound (reloadSoundsEmpty));
+        anim.Play(reloadEmptyAnim.name);
+        yield return new WaitForSeconds(reloadEmptyAnim.length / 3);
+        bulletsLeft = bulletsPerMag;
+        magsLeft--;
 
-		//canAims [0] = true;
-		//canFires [0] = true;
-		//canReloads [0] = true;
-		reloading = false;
-	}
+        //canAims [0] = true;
+        //canFires [0] = true;
+        //canReloads [0] = true;
+        reloading = false;
+    }
 
-	/*
+    /*
 	IEnumerator ReloadingSound (ReloadSound[] theSound)
 	{
 		foreach (ReloadSound lol in theSound) {
@@ -366,7 +385,7 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	/*
+    /*
 	IEnumerator CheckBools ()
 	{
 		CheckAim ();
@@ -377,14 +396,14 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	IEnumerator MuzzleFlash ()
-	{
-		muzzle.SetActive (true);
-		yield return new WaitForSeconds (0.1f);
-		muzzle.SetActive (false);
-	}
+    IEnumerator MuzzleFlash()
+    {
+        muzzle.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        muzzle.SetActive(false);
+    }
 
-	/*
+    /*
 	void CheckAim ()
 	{
 		canAim = false;
@@ -396,7 +415,7 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	/*
+    /*
 	void CheckReload ()
 	{
 		canReload = false;
@@ -408,7 +427,7 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	/*
+    /*
 	void CheckFire ()
 	{
 
@@ -422,21 +441,21 @@ public class NEATWeapon : MonoBehaviour
 	}
 	*/
 
-	IEnumerator Draw ()
-	{
-		//canAims [0] = false;
-		//canFires [0] = false;
-		//canReloads [0] = false;
-		//localSource.clip = drawSound;
-		//localSource.Play();
-		//StartCoroutine (ReloadingSound (drawSound));
-		anim [drawAnim.name].speed = 3;
-		anim.Play (drawAnim.name);
-		yield return new WaitForSeconds (drawAnim.length / 3);
-		//canAims [0] = true;
-		//canFires [0] = true;
-		canFire = true;
-		//canReload = true;
-		//canReloads [0] = true;
-	}
+    IEnumerator Draw()
+    {
+        //canAims [0] = false;
+        //canFires [0] = false;
+        //canReloads [0] = false;
+        //localSource.clip = drawSound;
+        //localSource.Play();
+        //StartCoroutine (ReloadingSound (drawSound));
+        anim[drawAnim.name].speed = 3;
+        anim.Play(drawAnim.name);
+        yield return new WaitForSeconds(drawAnim.length / 3);
+        //canAims [0] = true;
+        //canFires [0] = true;
+
+        //canReload = true;
+        //canReloads [0] = true;
+    }
 }
