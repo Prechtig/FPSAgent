1e3b6979ddf4968ec7e92c6857dbaaeeb875c1aa
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2016-12-14 16:44:07 +0100

    Updates LayerActivator

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/LayerActivator.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/executables/LayerActivator.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
 import org.mma.imagerecognition.dao.FileSystemDAO;
@@ -26,23 +29,18 @@ public class LayerActivator {
 			System.exit(1);
 		}
 		
-		File networkFile = null;
-		int imageId = Integer.parseInt(args[1]);
+		File networkFile = Paths.get("models", "continuous", "model2.bin").toFile();
+		int[] imageIds = new int[] { 47, 52, 55, 139, 146, 192, 193, 225, 242 };
 		
-		if(NumberUtils.isDigits(args[0])) {
-			int modelNumber = Integer.parseInt(args[0]);
-			networkFile = FileSystemDAO.getContinuousFolder().resolve(String.format("model%d.bin",modelNumber)).toFile();
-		} else {
-			String path = args[0];
-			networkFile = new File(path);
+		for(int imageId : imageIds) {
+			getActivationOfLayers(networkFile, imageId);
 		}
-		getActivationOfLayers(networkFile, imageId);
 	}
 	
 	public static void getActivationOfLayers(File networkFile, int imageId) throws FileNotFoundException, IOException {
 		MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(new FileInputStream(networkFile));
 		TrainingData image = TrainingDbDao.getImages(TrainingDbDao.getIndices(imageId, imageId)).get(0);
-		ImageTool.printColoredPngImage(image.getPixelData(), image.getWidth(), new File("image.png"));
+		ImageTool.printColoredPngImage(image.getPixelData(), image.getWidth(), new File("image"+ imageId +".png"));
 		
 		network.output(Nd4j.create(ImageTool.toScaledDoubles(image.getPixelData()), new int[] { 1, 3, image.getWidth(), image.getHeight()}), false);
 		int convLayers = NNTool.numberOfConvolutionalLayers(network);
@@ -52,13 +50,34 @@ public class LayerActivator {
 			persistFeatureMap(network.getLayer(currentLayer).activate().slice(0), currentLayer);
 		}
 		
+		System.out.println();
+		System.out.println("Evaluating image with id: " + imageId);
+		
 		//Print dense layer and output layer activation
+		double[] output = null;
 		for(int currentLayer = convLayers; currentLayer < network.getLayers().length; currentLayer++) {
-			System.out.println("Output of layer: " + currentLayer);
-			System.out.println(Arrays.toString(INDArrayTool.toFlatDoubleArray(network.getLayer(currentLayer).activate())));
+			Layer layer = network.getLayer(currentLayer);
+			INDArray activate = layer.activate();
+			if(currentLayer+1 == network.getLayers().length) {
+				// Output layer
+				output = INDArrayTool.toFlatDoubleArray(activate);
+			}
 		}
 		
+		System.out.println("Output of output layer:");
+		System.out.println(Arrays.toString(output));
+		System.out.println("Output of grount truths:");
 		System.out.println(Arrays.toString(image.getFeatures()));
+		
+		Pair<Integer, Double> featureInfo = findHighestIdAndValue(image.getFeatures());
+		System.out.println("Correct index = " + featureInfo.getLeft());
+		
+		Pair<Integer, Double> networkInfo = findHighestIdAndValue(output);
+		System.out.println("Guessed index = " + networkInfo.getLeft());
+		System.out.println("Guessed percentage = " + networkInfo.getRight());
+		System.out.println("Percentage of correct index = " + output[featureInfo.getLeft()]);
+		
+		System.out.println();
 	}
 	
 	private static void persistFeatureMap(INDArray featureMaps, int layer) throws IOException {
@@ -66,4 +85,26 @@ public class LayerActivator {
 			ImageTool.printGreyScalePngImage(INDArrayTool.toFlatDoubleArray(featureMaps.slice(featureMap)), featureMaps.shape()[1], FileSystemDAO.getFeatureMapsFolderForLayer(layer).resolve("featureMap" + featureMap + ".png").toFile());
 		}
 	}
+	
+	private static Pair<Integer, Double> findHighestIdAndValue(double[] arr) {
+		int idx = 0;
+		double val = 0d;
+		for(int i = 0; i < arr.length; i++) {
+			if(val < arr[i]) {
+				val = arr[i];
+				idx = i;
+			}
+		}
+		return new ImmutablePair<Integer, Double>(idx, val);
+	}
 }
+
+
+
+
+
+
+
+
+
+
