3d34527852ffd3e85b7696651657a37862d67cd1
Andreas Precht Poulsen <andreas.p.poulsen@gmail.com>
2016-12-16 09:46:03 +0100

    Changes to light ultra deep conv neet full training data

--- SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/ContinuousTraining.java
+++ SupervisedLearning/src/main/java/org/mma/imagerecognition/configuration/ContinuousTraining.java
@@ -68,7 +68,7 @@ public abstract class ContinuousTraining implements Trainable {
 	}
 	
 	protected void initConfig() {
-		configuration = BuilderFactory.getDeepConvNet(height, width, featureCount).build();
+		configuration = BuilderFactory.getUltraDeepConvNet(height, width, featureCount).build();
 	}
 	
 	protected void initNetwork() throws FileNotFoundException, IOException {
